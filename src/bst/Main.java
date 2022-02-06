54c9b0e47f31aee8c80c856f4bf690424ad9ab6d
amit.padaliya <amit.padaliya@techmojo.in>
2022-02-06 18:01:23 +0530

    done

--- src/bst/Main.java
+++ src/bst/Main.java
@@ -3,7 +3,16 @@ package bst;
 public class Main {
     public static void main(String[] args) {
         BST bst = new BST();
-        Node root = new Node();
-        bst.insert(root,7);
+        Node root = bst.insert(null,77);
+        bst.insert(root,12);
+        bst.insert(root,84);
+        bst.insert(root,789);
+        bst.insert(root,1);
+
+        System.out.println("root: " + root.data);
+        System.out.println("root.left: " + root.left.data);
+        System.out.println("root.right: " + root.right.data);
+        System.out.println("root.right.right: " + root.right.right.data);
+        System.out.println("root.left.left: " + root.left.left.data);
     }
 }
---
+++ src/bt/Main.java
@@ -0,0 +1,11 @@
+package bt;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        BinaryTree binaryTree = new BinaryTree();
+        Scanner s = new Scanner(System.in);
+        binaryTree.takeInput(s);
+    }
+}
